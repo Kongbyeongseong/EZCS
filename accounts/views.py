75e0f4876a3e09715abe792089d7bdaa767c4387
Kong <qudtjd567@naver.com>
2024-06-30 19:11:12 +0900

    .

--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-# from django.shortcuts import render
-
-# def list(request):
-#     return render(request, 'accounts/Login.html')
-
-from django.contrib.auth import login, authenticate, logout
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-
-def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')  # 회원가입 후 리디렉션할 페이지
-    else:
-        form = UserCreationForm()
-    return render(request, 'accounts/signup.html', {'form': form})
-
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect('home')  # 로그인 후 리디렉션할 페이지
-    else:
-        form = AuthenticationForm()
-    return render(request, 'accounts/login.html', {'form': form})
-
-def logout_view(request):
-    if request.method == 'POST':
-        logout(request)
-        return redirect('home')  # 로그아웃 후 리디렉션할 페이지
-=======
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import User
@@ -66,4 +29,3 @@ def login(request):
 def logout(request):
     request.session.pop('user')
     return redirect('/')
->>>>>>> 8e63f706690a8a255cbdf7dcb00b2165fa563487
